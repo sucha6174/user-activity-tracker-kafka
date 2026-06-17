69ae5028abb9cc231e14962e88fc78b835f12c98
sucha6174 <23mh1a4426@acoe.edu.in>
2026-06-17 22:18:00 +0530

    Add explicit Kafka listener container factory

---
+++ src/main/java/com/example/activitytracker/config/KafkaConsumerConfig.java
@@ -0,0 +1,24 @@
+package com.example.activitytracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object>
+    kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory
+    ) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
+    }
+}
